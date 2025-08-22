2c9b68faa5551aad2cebd56348ffaa0f97b83b54
BryanTYTan <90520318+BryanTYTan@users.noreply.github.com>
2025-08-22 20:45:59 +0800

    Drafted login Functionality

--- Flask_app/app.py
+++ Flask_app/app.py
@@ -1,5 +1,6 @@
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect, url_for
+import re
 
 app = Flask(__name__)
 
@@ -8,10 +9,63 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
-
 @app.route('/')
-def index():
-    conn = get_db_connection()
-    users = conn.execute('SELECT * FROM User').fetchall()
-    conn.close()
-    return render_template('index.html', users=users)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM User WHERE username = %s AND password = %s', (username, password))
+        account = cursor.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            return render_template('index.html', msg='Logged in successfully!')
+        else:
+            msg = 'Incorrect username/password!'
+    
+        conn.close()
+        
+    return render_template('login.html', msg=msg)
+
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    return redirect(url_for('login'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
+        account = cursor.fetchone()
+        if account:
+            msg = 'Account already exists!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address!'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only letters and numbers!'
+        elif not username or not password or not email:
+            msg = 'Please fill out the form!'
+        else:
+            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, email))
+            conn.commit()
+            msg = 'You have successfully registered!'
+            
+        conn.close()
+        
+    return render_template('register.html', msg=msg)
+
+if __name__ == '__main__':
+    app.run(debug=True)
